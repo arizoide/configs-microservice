d37500d42b7c6fea30783370b3dd0986ad374355
arizoide <ari.thomazini@gmail.com>
2022-02-28 16:18:24 -0300

    Initial commit

---
+++ src/test/java/br/com/arithomazini/config/microservice/configs/microservice/ConfigsMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.arithomazini.config.microservice.configs.microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigsMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
